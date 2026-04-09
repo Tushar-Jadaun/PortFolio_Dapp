241636e67cd9f274c760b6dd3557a3d10d187545
Tushar-Jadaun <tusharjadaun2001485@gmail.com>
2026-04-09 21:34:06 +0530

    how to address the wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 
  
 export function App() {
+  const[wallet,setWallet]=useState<string[]>([])
   return (
     <div >
       <b>
-
+          <button onClick={()=>{
+            if(window.backpack){
+              setWallet(w=>[...w,"backpack"])
+            }
+            if(window.solflare){
+              setWallet(w=>[...w,"solflare"])
+            }
+            if(window.phantom){
+              setWallet(e=>[...e,"phantom"])
+            }
+          }
+            }>Connect with you wallet</button>
       </b>
+      <div>
+        {wallet.map(wallet=><button>{wallet}</button>)}
+      </div>
     </div>
   );
 }
